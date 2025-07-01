12ea7b91fef0ad15a51a161dbc2dec0b61c6c42e
imbatmanfrl <batmansofficialmail@gmail.com>
2025-07-01 19:00:19 +0100

    we're gettng there!

--- percentagegain.py
+++ percentagegain.py
@@ -1,26 +1,34 @@
 bias = input("LONG or SHORT?: ").upper()
-account_balance = input ("Account Balance?: ")
-lots = input("Lots?: ")
+# I want to modify the program in a way where it asks for what pair, and based on what pair for usd it will mulitply pips by 10,000
+#and if it were a jpy pair it would multiply by 100 I just need where ill store pair, maybe inside a 2D list
+
+fav_pairs = ["GBPUSD","EURUSD","GBPJPY","ES"]
+pair = input("What pair?: ")
+account_balance = float (input ("Account Balance?: "))
+lots = float (input("Lots?: "))
 profit = ""
-entry_price = input("Entry Price?: ")
-exit_Price = input("Exit Price?: ")
-earned_pips = ""
+entry_price = float(input("Entry Price?: "))
+exit_Price = float(input("Exit Price?: "))
+pips = ""
 percentage_gain = ""
 
 if bias == "LONG":
-    earned_pips = exit_Price - entry_price
-    profit = lots * earned_pips
+    pips = exit_Price - entry_price
+    profit = lots * pips
     percentage_gain = (profit/account_balance)*100
-    print(earned_pips)
-    print("Your profit is: "+profit)
-    print("You earned: "+ percentage_gain)
+    if pair == "GBPJPY" in fav_pairs:
+        pips = pips*100
+        print("Total pips: " + str(pips))
+    elif
+    print("Your profit is: "+ str(profit))
+    print("You earned: "+ str (percentage_gain))
 
 elif bias == "SHORT":
-    earned_pips = entry_price - entry_price
-    profit = lots * earned_pips
+    pips = entry_price - entry_price
+    profit = lots * pips
     percentage_gain = (profit / account_balance) * 100
-    print(earned_pips)
-    print("Your profit is: " + profit)
-    print("You earned: " + percentage_gain)
+    print("Total pips: " + str(pips))
+    print("Your profit is: $" + str(profit))
+    print("You earned: " + str(percentage_gain)+"%")
 
 
