47ac8ec87fca12672decf205682eaa8513de48fc
imbatmanfrl <batmansofficialmail@gmail.com>
2025-07-02 18:28:41 +0100

    I learnt more about functions

--- Fucntions.py
+++ Fucntions.py
@@ -1,11 +1,21 @@
 # A block of code executed only when it is called
 
-def Batman(first_name,last_name):
-    print("🦇" +first_name+ last_name )
-    print("I'm going to save you")
+#def Batman(first_name,last_name):
+#    print("🦇" +first_name+ last_name )
+#    print("I'm going to save you")
 
 #my_name = "Awwal"
 #Batman(my_name)
 #Batman("awwal",21)
 
-Batman("The ","Batman")
+#Batman("The ","Batman")
+
+def Batman():
+    first_name = input("Your First Name?: ")
+    last_name = input("Your Last Name?:")
+    full_name = first_name + last_name
+    return full_name
+#    return ("Your name is:" + first_name + last_name)
+#    return first_name and last_name
+
+print(Batman())
--- percentagegain.py
+++ percentagegain.py
@@ -9,46 +9,49 @@ lots = float (input("Lots?: "))
 profit = ""
 entry_price = float(input("Entry Price?: "))
 exit_Price = float(input("Exit Price?: "))
-pips = ""
-pip_value =""
-percentage_gain = ""
-
-if bias == "LONG":
-    pips = exit_Price - entry_price
-    if pair == "GBPJPY" in fav_pairs:
-        pips = pips * 100
-        pip_value = lots * 10 # 0.01 lots = 0.1 lots on jpy pairs
-        profit = pips * pip_value
-        percentage_gain = (profit / account_balance) * 100
-        print("Your profit is: " + str(round(profit, 2)))
-        print("You earned: " + str(round(percentage_gain, 2)) + "%")
-        print("Total pips: " + str(round (pips,2)))#this will round pps up to 2 decimal places
-    elif pair == "GBPUSD" or "EURUSD" in fav_pairs:
-        pips = pips * 10000
-        pip_value = lots * 10  # 0.01 lots = 0.01 lots on usd pairs
-        profit = pips * pip_value
-        percentage_gain = (profit / account_balance) * 100
-        print("Your profit is: " + str(round(profit, 2)))
-        print("You earned: " + str(round(percentage_gain, 2)) + "%")
-        print("Total pips: " + str(round(pips, 2)))
 
 
-elif bias == "SHORT":
-    pips = entry_price - exit_Price
-    if pair == "GBPJPY" in fav_pairs:
-        pips = pips * 100
-        pip_value = lots * 10  # 0.01 lots = 0.1 lots on jpy pairs
-        profit = pips * pip_value
-        percentage_gain = (profit / account_balance) * 100
-        print("Your profit is: " + str(round(profit, 2)))
-        print("You earned: " + str(round(percentage_gain, 2)) + "%")
-        print("Total pips: " + str(round(pips, 2)))  # this will round pps up to 2 decimal places
-    elif pair == "GBPUSD" or "EURUSD" in fav_pairs:
-        pips = pips * 10000
-        pip_value = lots * 10 # 0.01 lots = 0.01 lots on usd pairs
-        profit = pips * pip_value
-        percentage_gain = (profit / account_balance) * 100
-        print("Your profit is: " + str(round(profit, 2)))
-        print("You earned: " + str(round(percentage_gain, 2)) + "%")
-        print("Total pips: " + str(round(pips, 2)))
+while True:
+    if bias == "LONG":
+        pips = exit_Price - entry_price
+        if pair == "GBPJPY" in fav_pairs:
+            pips = pips * 100
+            pip_value = lots * 10  # 0.01 lots = 0.1 lots on jpy pairs
+            profit = pips * pip_value
+            percentage_gain = (profit / account_balance) * 100
+            print("Your profit is: " + str(round(profit, 2)))
+            print("You earned: " + str(round(percentage_gain, 2)) + "%")
+            print("Total pips: " + str(round(pips, 2)))  # this will round pps up to 2 decimal places
+        elif pair == "GBPUSD" or "EURUSD" in fav_pairs:
+            pips = pips * 10000
+            pip_value = lots * 10  # 0.01 lots = 0.01 lots on usd pairs
+            profit = pips * pip_value
+            percentage_gain = (profit / account_balance) * 100
+            print("Your profit is: " + str(round(profit, 2)))
+            print("You earned: " + str(round(percentage_gain, 2)) + "%")
+            print("Total pips: " + str(round(pips, 2)))
+
+
+    elif bias == "SHORT":
+        pips = entry_price - exit_Price
+        if pair == "GBPJPY" in fav_pairs:
+            pips = pips * 100
+            pip_value = lots * 10  # 0.01 lots = 0.1 lots on jpy pairs
+            profit = pips * pip_value
+            percentage_gain = (profit / account_balance) * 100
+            print("Your profit is: " + str(round(profit, 2)))
+            print("You earned: " + str(round(percentage_gain, 2)) + "%")
+            print("Total pips: " + str(round(pips, 2)))  # this will round pps up to 2 decimal places
+        elif pair == "GBPUSD" or "EURUSD" in fav_pairs:
+            pips = pips * 10000
+            pip_value = lots * 10  # 0.01 lots = 0.01 lots on usd pairs
+            profit = pips * pip_value
+            percentage_gain = (profit / account_balance) * 100
+            print("Your profit is: " + str(round(profit, 2)))
+            print("You earned: " + str(round(percentage_gain, 2)) + "%")
+            print("Total pips: " + str(round(pips, 2)))
+
+    elif bias == "Stop":
+        print("End of session")
+
 
