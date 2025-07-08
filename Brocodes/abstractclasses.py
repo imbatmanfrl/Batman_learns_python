b94269f30a414e2a6c02d935131af2c5e8fb4b0b
imbatmanfrl <batmansofficialmail@gmail.com>
2025-07-08 12:16:23 +0100

    abstract class

--- Brocodes/abstractclasses.py
+++ Brocodes/abstractclasses.py
@@ -3,3 +3,33 @@
 
 abstract class = a class containing one or more abstract methods
 abstract method = a method that has a declaration but does not have an implementation"""
+
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def go(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+class Car(Vehicle):
+
+    def go(self):
+        print("You drive the car")
+
+class Motorcycle(Vehicle):
+#the children class always carry the functions and methods in the abstract class
+    def go(self):
+        print("You ride the motorcycle")
+
+#vehicle = Vehicle()
+car = Car()
+motorcycle = Motorcycle()
+
+#vehicle.go()
+car.go()
+motorcycle.go()
