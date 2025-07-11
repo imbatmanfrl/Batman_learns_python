59e4f09a433a07701221cfc93ba823d2f0bd7647
imbatmanfrl <batmansofficialmail@gmail.com>
2025-07-11 02:24:24 +0100

    A day closer to

--- MiniAmaterasuBase/roughtwork.py
+++ MiniAmaterasuBase/roughtwork.py
@@ -39,38 +39,54 @@ print(budget.total_earned(1))
 budget.expense_list.append("Spotify")
 print(budget.expense_list)"""
 
+from budgetModel import BudgetModel
+
 import datetime
 class WeeksPassed:
 
-    def __init__(self,weeks):
+    """def __init__(self,weeks):
+# this is used to receive the week you think it is since you started using the budget model
+the constructor is not needed
         self.weeks = weeks
-
+"""
     def last_saved(self):
+#this is used to display the days current date
         date = datetime.date.today()
         return date
+# now the program should be structures=d in a way that the stored function doesnt run,
+    # until someone has successfully logged in a budget
 
+    class BudgetModel:
+        def __init__(self, weekly_earnings, weekly_expenditure, weekly_savings, weekly_investments, ):
+            self.weekly_earnings = weekly_earnings
+            self.weekly_expenditure = weekly_expenditure
+            self.weekly_savings = weekly_savings
+            self.weekly_investments = weekly_investments
     def store(self):
+#this stores that date in an external txt file
         stored = self.last_saved()
         with open("last_update.txt", "w") as file:
             file.write(str(stored))
         print(self.last_saved())
 
-    from datetime import datetime, date
+
     def time_since_last_update(self):
+#this now turns the date of that external txt file into a data type which can be subtracted from todays date to show
+#you how ong it has been since you logged in a budget
         try:
-            with open("last_update.txt","r") as file:
+            with open("last_update.txt", "r") as file:
                 stored_date_str = file.read().strip()
-                stored_date = datetime.strptime(stored_date_str,"%Y-%m-%d").date()
+                stored_date = datetime.datetime.strptime(stored_date_str,"%Y-%m-%d").date()
                 today = datetime.date.today()
-                days_passed = today - stored_date
+                days_passed = (today - stored_date)//7
                 return f"{days_passed} days has passed since you last budgeted!"
         except FileNotFoundError:
             print("No saved date appeared yet!")
-            """calc = int(updated) - int(todays_date)
-            return calc
-        finally:
-            print(f"This is when you last updated your budget {self.time_since_last_update()}")"""
+#all that is left now is for the user program to be able to update the last_saved function,
+# as it is it'll be using the current date of every day as the last saved.
+# we want to be able to append that file or idk i think im right
 
-weeks_passed = WeeksPassed(2)
+budget = BudgetModel(10000,2000,5000,3000)
+weeks_passed = WeeksPassed()
 weeks_passed.store()
 weeks_passed.time_since_last_update()
