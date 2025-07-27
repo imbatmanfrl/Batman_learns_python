e31153f1b9606c8730f4940bc9b055ecdab650fa
imbatmanfrl <batmansofficialmail@gmail.com>
2025-07-27 18:00:27 +0100

    skill issue

--- MiniAmaterasu1/TrackPair.py
+++ MiniAmaterasu1/TrackPair.py
@@ -1,54 +1,37 @@
 import json
 import requests
 
-
-from GetData import Api
-
-class Track(Api):
-
-    def particular_pair(self):
-
-        chainId = input(f"Enter the chainId: ")
-        tokenAddress = input(f"Enter the tokenAddress: ")
+class Track:
+    def particular_asset(self,chainId,tokenAddress):
         url = f"https://api.dexscreener.com/token-pairs/v1/{chainId}/{tokenAddress}"
         response = requests.get(url)
         data = response.json()
 
-        with open("currentTokenomics.json", "w") as file:
-            json.dump(data, file, indent=2)
-            for item in data:
-                print(item)
+        with open("currentTokenomics.json","w") as file:
+            json.dump(data,file,indent=2)
 
-    def tracker(self):
-        pair_info = []
-        with open("currentTokenomics.json", "r") as file:
+    def tracking(self):
+        tokenomics = []
+
+        with open("currentTokenomics.json","r") as file:
             data = json.load(file)
+            for pairs in data:
+                info = {
+                    "name": pairs["baseToken"]["name"],
+                    "symbol": pairs["baseToken"]["symbol"],
+                    "chainId": pairs.get("chainId"),
+                    "address": pairs.get("pairAddress"),
+                    "price": pairs.get("priceUsd"),
+                    "market_cap": pairs.get("fdv"),
+                    "liquidity": pairs.get("liquidity"),
+                    "volume": pairs.get("volume")
+                }
 
-            if isinstance(data,dict) and "pair" in data:
-                for item in data:
-                    pairs = item.get("pair")
-                    if not pairs:
-                        continue
-                    tokenomics = {
-                        "name": pairs["baseToken"]["name"],
-                        "symbol": pairs["baseToken"]["symbol"],
-                        "chainId": pairs.get("chainId"),
-                        "address": pairs.get("pairAddress"),
-                        "price": pairs.get("priceUsd"),
-                        "market_cap": pairs.get("fdv"),
-                        "liquidity": pairs.get("liquidity"),
-                        "volume": pairs.get("volume")
-                    }
-
-                    pair_info.append(tokenomics)
-        with open("pair_info.json", "w") as file:
-            json.dump(pair_info, file, indent=2)
-
-    def start_tracking(self):
-        self.particular_pair()
-        self.tracker()
+                tokenomics.append(info)
 
 
-tracker = Track("solana","ELQrbSfE9J2xPnxDGGfboGchw8QY7ua7NjsDtJYi4qt9")
+track = Track()
+track.particular_asset("solana","ELQrbSfE9J2xPnxDGGfboGchw8QY7ua7NjsDtJYi4qt9")
+track.tracking()
 #tracker = Track("solana","ELQrbSfE9J2xPnxDGGfboGchw8QY7ua7NjsDtJYi4qt9")
-tracker.start_tracking()
+
